3f053c03fe23c1cda8b739f6dbafbf8d0d520065
dormanno <dormanno@gmail.com>
2025-09-14 19:21:59 +0200

    Isolation Forest for Time series data

---
+++ ColumnNames.py
@@ -0,0 +1,11 @@
+TRADE = "TradeName"
+DATE = "Date"
+START = "Start_PV"
+END = "End_PV"
+TOTAL = "Total_PnL"
+UNEXPLAINED = "Residual"
+TOTAL_JUMP = "TotalJump"
+UNEXPLAINED_JUMP = "ResidualJump"
+PNL_SLICES = [
+    "Basis_CoF_PnL", "Recovery_Rate_PnL", "Rates_PnL", "Model_PnL", "Basis_CoT_PnL", "Credit_Index_PnL", "Credit_Single_PnL"
+]
